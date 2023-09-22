0758ee5ab13c1d4b874b307a5787ffb12aeca900
Rama Prawira <ramaprawira727@gmail.com>
2023-09-22 10:01:59 +0700

    score 5/5

--- main.py
+++ main.py
@@ -4,69 +4,65 @@ import string
 class Stack():
     def __init__(self):
         self.items = []
-    def push(self,item):
-        self.items.append(item)
+    def push(self, item):
+        self.items.append(item)  
     def pop(self):
-        return self.items.pop()
+        return self.items.pop() 
     def size(self):
         return len(self.items)
     def is_empty(self):
-        return len(self.items) == 0
+        return len(self.items) == 0 
     def peek(self):
         return self.items[-1]
-    
-def reverse(equation):
-    
         
 def order(operator):
-    if operator == '(' or operator ==')':
+    if operator == '(' or operator == ')':
         return 3
     elif operator == '^':
         return 2
     elif operator == '*' or operator == '/':
         return 1
     elif operator == '-' or operator == '+':
-        return 0
-    
-def infix2prefix(infix):
-    opStack = Stack()
-    outStack = Stack()
-    aux_stack = Stack()
-    infixrev = reverse(infix)
-
-    for chr in infixrev:
-        if (chr.isalnum() == True):
-            outStack.push(chr)
-        elif(chr.isalpha() == True):
-            outStack.push(chr)
+        return 1
+        
+def reverse(string):
+    rev = []
+    for chr in string [::-1]:
+        if chr == ')':
+            rev.append('(')
         elif chr == '(':
-            opStack.push(chr)
-        elif ((chr == '+') or (chr == '-') or (chr == '*') or (chr == '/')) and opStack.is_empty() == True:
-            opStack.push(chr)
-        elif ((chr == '+') or (chr == '-') or (chr == '*') or (chr == '/')) and opStack.is_empty() == False:
-            if order(opStack.peek())>order(chr):
-                opStack.push(chr)
-            elif order(opStack.peek())<=order(chr):
-                outStack.push(opStack.pop())
-                opStack.push(chr)
-        elif chr == ")":
-            if opStack.is_empty() == True:
-                opStack.push(chr)
-            if opStack.is_empty() == False:
-                while opStack.peek != '(':
-                    outStack.push(opStack.pop())
+            rev.append(')')
+        else:
+            rev.append(chr)
+    return rev
     
-    for item in opStack.items:
-        outStack.push(opStack.pop())
-
-    while outStack.is_empty() == False:
-        print(outStack.pop(),end='')
-
-
-A = "(9+1)*(((1+2)+3)+4)"
-revA = reverse(A)
-print(revA)
-
-"""
-    (9+1)*(((1+2)+3)+4)
-"""
+def convert(infix):
+    op = Stack()
+    num = Stack()
+    aux = reverse(infix)
+    for chr in aux:
+        if (chr.isalnum()==True):
+            num.push(chr)
+        elif (chr.isalpha()==True):
+            num.push(chr)
+        elif chr == '(':
+            op.push(chr)
+        elif ((chr=='+')or(chr=='-')or(chr=='*')or(chr=='/')) and op.is_empty()==True:
+            op.push(chr)
+        elif ((chr=='+')or(chr=='-')or(chr=='*')or(chr=='/')) and op.is_empty()==False:
+            if order(op.peek())>order(chr):
+                op.push(chr)
+            elif order(op.peek())<=order(chr):
+                num.push(op.pop())
+                op.push(chr)
+        elif chr == ')':
+            while op.peek() != '(':
+                num.push(op.pop())
+            op.pop()           
+    for item in op.items:
+        num.push(op.pop())
+    while num.is_empty() == False:
+        print(num.pop(),end="")
+                
+inp = input()
+convert(inp)
